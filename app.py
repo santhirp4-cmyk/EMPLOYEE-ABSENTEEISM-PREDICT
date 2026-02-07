9c55b15f6d7e114d9dbd4ebbff900d2396b5810f
santhirp4-cmyk <santhirp4@gmail.com>
2026-02-07 14:25:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-# Step 1: Import Streamlit
-import streamlit as st
 
-# Step 2: Title of your app
-st.title("My First Streamlit App")
-
-# Step 3: Write a description
-st.write("Hello! This is my first project using Streamlit.")
-
-# Step 4: Get input from user
-name = st.text_input("Enter your name:")
-
-# Step 5: Display input
-st.write("Hello,", name)
-
-# Step 6: Create a button
-if st.button("Click me"):
-    st.write("You clicked the button!")
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -36,4 +19,5 @@ chart_data = pd.DataFrame(
 )
 
 st.line_chart(chart_data)
+
 
